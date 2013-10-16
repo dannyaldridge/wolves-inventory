eed0356fe3f90a7863f00cb242c6a9d4e8d10b01
Danny Aldridge <dannyaldridge@gmail.com>
2013-10-16 11:24:08 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WolvesInventory::Application.config.secret_key_base = '0374eeb1249bc72b885bc1152ee1c0f4b13c5dfaeeb2f5f2540bcf8865e5b7eea71f5c852c20db64d69c982389fad9026d55454d8b272a9195dc6a9237958bbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WolvesInventory::Application.config.session_store :cookie_store, key: '_wolves-inventory_session'
